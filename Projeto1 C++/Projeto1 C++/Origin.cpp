cfc9e62083012dd9de2febd6e2eebb001c68eafe
Isaac Adler <isaac.adler@ufrpe.br>
2024-04-27 21:08:41 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto1 C++/Projeto1 C++/Origin.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Primeiro código em C++" << std::endl;
+	system("PAUSE");
+	return 0;
+	
+}
---
+++ Teste/Teste/Primeiro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//A diretiva de pré-processador #include
+//indica para incluir o arquivo de cabeçalho
+//iostream
+
+int main() {
+//função principal. Todo programa C++ começa
+//com essa função principal main()
+	std::cout << "Primeiro codigo" << std::endl;
+	system("PAUSE");
+	return 0;
+}
